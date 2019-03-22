e60abf25ac65effb41814fc20fc0f468fb4dc724
Jordan Irwin <antumdeluge@gmail.com>
2019-03-22 00:00:56 -0700

    Add py.md module for converting markdown input to plain text (WIP)

---
+++ py/md.py
@@ -0,0 +1,19 @@
+
+### Markdown input handling.
+#
+# @package py.md
+
+
+### Converts markdown formatted text to plain text.
+#
+# @function markdownToText
+# @tparam str input Markdown formatted text.
+# @treturn str Re-formatted input to plain text.
+def markdownToText(md_in):
+	txt_out = md_in
+
+	# handle braces
+	idx_l = txt_out.index('[')
+	idx_r = txt_out.index(']')
+	while '[' in txt_out and ('][' in txt_out or '](' in txt_out) and idx_l < idx_r:
+		break
